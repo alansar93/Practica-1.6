b2df817845faf864a2a693114b22372db749e86b
Alan  Sarchman <alansarhchman@live.com.argit config --global user.emailgit config --globalgit config --global config user.namegit initgit config --global user.email alansarhchman@live.com.ar>
2023-04-03 19:24:42 -0300

    Con Facu

---
+++ algoritmo.js
@@ -0,0 +1,8 @@
+/* let variable = "Nombre de Usuario"; */
+let variable = prompt("Nombre de Usuario");
+document.write("<h1> Hola" + " " + variable + "</h1>");
+
+
+
+
+/* Trabajo realizado por Alan y Facundo  */
