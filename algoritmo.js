7b03085bcff5101c969a8a3dcb08be4f1faa1ff9
Alan  Sarchman <alansarhchman@live.com.argit config --global user.emailgit config --globalgit config --global config user.namegit initgit config --global user.email alansarhchman@live.com.ar>
2023-04-03 19:53:29 -0300

    nueva version

--- algoritmo.js
+++ algoritmo.js
@@ -1,6 +1,23 @@
-/* let variable = "Nombre de Usuario"; */
-let variable = prompt("Nombre de Usuario");
-document.write("<h1> Hola" + " " + variable + "</h1>");
+/* let Nombre = "Nombre de Persona"; */
+let Nombre = prompt("Nombre de Persona");
+document.write("<h1> Hola" + " " + Nombre + "</h1>");
+
+/* let Edad = "Edad de la persona"; */
+let Edad = prompt("Edad de la Persona");
+document.write("<h1>" + "Tenes" + " " + Edad + " " + "años" + "</h1>");
+
+/* let Peso = "Peso de la Persona" */
+let Peso = prompt("Peso Persona con Decimales");
+document.write("<h1>" + "Tu" + " " + "peso" + " " + "es" + " " + "de" + " " + Peso + "Kg" + "</h1>");
+
+/* let Email = "Email Persona" */
+let Email = prompt("Email Persona");
+document.write("<h1>" + "Tu" + " " + "email" + " " + "es" + " " + Email + "</h1>" );
+
+/* let Sexo = "Sexo Persona" */
+let Hijo = prompt("Tenes Hijos? F verdadero, N negativo");
+document.write("<h1>" + "tu" + " " + "respuesta" + " " + "es" + " " + Hijo);
+
 
 
 
